687ebef16d34a0708be2983e862f66b438533399
Yuri Linux <07yuri@gmail.com>
2022-02-11 00:32:16 -0500

    excercises upto function

--- conditionals-whichschool.js
+++ conditionals-whichschool.js
@@ -1,9 +1,13 @@
 let whichSchool = (age) => {
+  //* function named "whichSchool" takes parameter "age"
   if (age < 13) {
+    /* if age is below 13, return the value of "Elementary school*/
     return "Elementary School";
   } else if (13 <= age && age <= 18) {
+    /* if the age is between 13 and 18 inclusive, return the value secondary school */
     return "Secondary School";
   } else {
+    //For all other values, return lihthouse labs
     return "Lighthouse Labs";
   }
 };
